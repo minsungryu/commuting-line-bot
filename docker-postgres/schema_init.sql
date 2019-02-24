b547bf3f07af42c549480fdd0c8abb5b2d8f11eb
유민성 <ryums0227@gmail.com>
2019-02-24 22:16:27 +0900

    fix dockerfile and docker-compose

---
+++ docker-postgres/schema_init.sql
@@ -0,0 +1,24 @@
+create table if not exists account
+(
+  id bigserial not null
+    constraint account_pk
+    primary key,
+  user_id varchar(64) not null,
+  deleted boolean default false,
+  attendance_time time default '09:30:00'::time without time zone,
+  leave_time time default '18:00:00'::time without time zone
+);
+
+comment on table account is '알람봇 사용자';
+
+comment on column account.id is 'PK';
+
+comment on column account.user_id is 'LINE user id';
+
+comment on column account.deleted is '삭제여부';
+
+comment on column account.attendance_time is '출근시간';
+
+comment on column account.leave_time is '퇴근시간';
+
+alter table account owner to line_alarm_bot;
